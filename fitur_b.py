48e615e1a1d16547792ac23eba88a64815cf9d5d
Salsabilla Adisty <salsabillaadisty31@gmail.com>
2023-03-21 07:28:47 +0700

    menambah 1 baris code di fitur_b.py

--- fitur_b.py
+++ fitur_b.py
@@ -1,1 +1,2 @@
-print("ini adalah halaman fitur b dari branch feature-b")
+print("ini adalah halaman fitur b dari branch feature-b dan feature c")
+print("halaman login harus menginputkan username dan password ya")
